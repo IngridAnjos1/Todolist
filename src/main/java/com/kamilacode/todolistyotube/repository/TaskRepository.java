f520fe1a1b368de8bcae1225276d6431217b276d
Ingrid Anjos <ingrid_anjos@hotmail.com>
2022-09-20 10:54:17 -0300

    adicao do repository

--- src/main/java/com/kamilacode/todolistyotube/repository/TaskRepository.java
+++ src/main/java/com/kamilacode/todolistyotube/repository/TaskRepository.java
@@ -1,4 +1,9 @@
 package com.kamilacode.todolistyotube.repository;
 
-public interface TaskRepository {
+import com.kamilacode.todolistyotube.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
