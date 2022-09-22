42965ce945ac27613d24ef04d5a6c6655888ff52
Ingrid Anjos <ingrid_anjos@hotmail.com>
2022-09-22 11:04:19 -0300

    adicao metodos post/get/put/delete

--- src/main/java/com/kamilacode/todolistyotube/controller/TaskController.java
+++ src/main/java/com/kamilacode/todolistyotube/controller/TaskController.java
@@ -1,4 +1,47 @@
 package com.kamilacode.todolistyotube.controller;
 
+import com.kamilacode.todolistyotube.model.Task;
+import com.kamilacode.todolistyotube.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
 public class TaskController {
+    @Autowired
+    TaskService taskService;
+
+    @PostMapping("/tasks")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Task createTask(@RequestBody Task task){
+        return taskService.createTask(task);
+    }
+
+    @GetMapping("/tasks")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Task>getAllTasks(){
+        return taskService.listAllTasks();
+    }
+
+    @GetMapping("/tasks/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Task>getTaskById(@PathVariable (value = "id")Long id){
+        return taskService.findTaskById(id);
+
+    }
+    @PutMapping("/tasks/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Task> updateTaskById(@PathVariable (value = "id") Long id, @RequestBody Task task){
+     return taskService.updateTaskById(task, id);
+    }
+    @DeleteMapping("/tasks/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Object>deleteTaskById(@PathVariable (value = "id") Long id){
+        return taskService.deleteById(id);
+    }
+
 }
